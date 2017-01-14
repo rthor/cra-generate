153cbfef40a8a8e4b323b67f088388a3485505e1
Ragnar Valgeirsson <ragnar.valgeirsson@gmail.com>
2017-01-14 16:51:48 +0000

    Fix error handling

---
+++ generate.js
@@ -0,0 +1,129 @@
+'use strict'
+
+const { EOL } = require('os')
+const changeCase = require('change-case')
+const fs = require('fs')
+const mkdir = require('mkpath')
+const chalk = require('chalk')
+const path = require('path')
+
+const allowedNameTransforms = {
+  camelCase: true,
+  constantCase: true,
+  headerCase: true,
+  paramCase: true,
+  pascalCase: true,
+  snakeCase: true,
+}
+
+function template(file) {
+  const filePath = path.resolve(__dirname, 'templates/component/', file)
+  return {
+    filePath,
+    content: fs.readFileSync(filePath, 'utf8'),
+  }
+}
+
+function save(componentName, fileName, filePath, content) {
+  const contents = content.replace(/\$Name\$/g, componentName).replace(/\$name\$/g, fileName)
+  fs.writeFile(filePath, contents, 'utf8')
+}
+
+function getComponentPath(componentName, directory, fileName) {
+  const cwd = process.cwd()
+  const root = path.join(cwd, 'src')
+  mkdir.sync(root)
+
+  const dir = path.join(root, directory)
+  mkdir.sync(dir)
+  const componentPath = path.join(dir, fileName)
+  const exists = fs.existsSync(componentPath)
+
+  if (exists) {
+    console.error(`Component [${componentName}] already exists at ${componentPath}`)
+    return process.exit(1)
+  }
+
+  mkdir.sync(componentPath)
+  return componentPath
+}
+
+function implementTypeChecking(typeSystem, content) {
+  let mutatedContent
+
+  switch (typeSystem) {
+    case 'flow':
+      mutatedContent = `// @flow${EOL}${EOL}${content}`
+      break
+    default:
+      mutatedContent = content
+      break
+  }
+
+  return {
+    content: mutatedContent,
+  }
+}
+
+function validTransform(transform) {
+  if (!allowedNameTransforms.hasOwnProperty(transform)) {
+    console.log('Invalid component name transform')
+    console.log('  allowed transform functions are:')
+    Object.keys(allowedNameTransforms).forEach(trf => {
+      console.log(`  - ${chalk.cyan(trf)}`)
+    })
+    process.exit(1)
+  }
+}
+
+function transformNames(component, fileFn, compFn) {
+  validTransform(fileFn)
+  validTransform(compFn)
+  const fileName = changeCase[fileFn](component)
+  const componentName = changeCase[compFn](component)
+  return { fileName, componentName }
+}
+
+function generate(component, options) {
+  const { fileName, componentName } = transformNames(component,
+    options.transform.fileName,
+    options.transform.component)
+  const componentPath = getComponentPath(componentName, options.directory, fileName)
+
+  const scriptFiles =[
+    template('index.js'),
+    template(options.isFunctional ? 'stateless.js' : 'stateful.js'),
+  ]
+  const styleFiles = [template('styles.css')]
+
+  scriptFiles.forEach(script => {
+    const { name } = path.parse(script.filePath)
+    const { content } = implementTypeChecking(options.typeCheck, script.content)
+    const filePath = path.join(componentPath,
+      `${name === 'index' ? 'index' : fileName}.js`
+    )
+    save(componentName, fileName, filePath, content)
+  })
+
+  styleFiles.forEach(style => {
+    const filePath = path.join(componentPath, 'styles.css')
+    save(componentName, fileName, filePath, style.content)
+  })
+}
+
+module.exports = generate
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const chalk = require('chalk')
 const fs = require('fs')
 const path = require('path')
 const mkpath = require('mkpath')
-const { EOL } = require('os')
+const generate = require('./generate')
 const { version } = require('./package.json')
 
 let component
@@ -14,7 +14,8 @@ let component
 const program = commander
   .version(version)
   .option('-f, --functional', 'create a functional component')
-  .option('-t, --use-flow', 'add @flow comment to script files')
+  .option('-t, --type-check [system]', 'add @flow comment to script files')
+  .option('-d, --directory [dir]', 'specify a directory for the component', 'components')
   .arguments('<component>')
   .action((c) => component = c)
   .parse(process.argv)
@@ -25,44 +26,12 @@ if (!component) {
   process.exit(1)
 }
 
-function captialized(word) {
-  const _word = '' + word
-  return _word.charAt(0).toUpperCase() + _word.slice(1)
-}
-
-function getFile(file) {
-  return fs.readFileSync(path.resolve(__dirname, 'templates/component/', file), 'utf8')
-}
-
-function saveFile(name, ext, file) {
-  const filePath = path.join(targetPath, `/${name}.${ext}`)
-  const contents = file.replace(/\$Name\$/g, Component).replace(/\$name\$/g, component)
-  fs.writeFile(filePath, contents, 'utf8')
-}
-
-const Component = captialized(component)
-const isPure = program.functional
-const useFlow = program.useFlow
-
-const targetPath = path.resolve(process.cwd(), 'src/components/', component)
-
-const jsFiles = {
-  index: getFile('index.js'),
-  [component]: getFile(isPure ? 'stateless.js' : 'stateful.js'),
-}
-
-const styles = getFile('styles.css')
-
-mkpath(targetPath, err => {
-  if (err) {
-    return process.exit(1)
-  }
-
-  Object.keys(jsFiles).forEach(file => {
-    let content = jsFiles[file]
-    if (useFlow) content = `// @flow${EOL}${EOL}${content}`
-    saveFile(file, 'js', content)
-  })
-
-  saveFile('styles', 'css', styles)
+generate(component, {
+  directory: program.directory,
+  typeCheck: program.typeCheck,
+  isFunctional: program.functional,
+  transform: {
+    fileName: 'paramCase',
+    component: 'pascalCase',
+  },
 })
