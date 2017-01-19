664d59f83927728cc03c5568be6ef5f0d931f1f3
Ragnar Valgeirsson <ragnar.valgeirsson@gmail.com>
2017-01-19 18:03:50 +0000

    Exit early if running on version prior to Node v4

---
+++ cli.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const commander = require('commander')
+const chalk = require('chalk')
+const getConfig = require('./config')
+const generate = require('./generate')
+const version = require('./package.json').version
+
+let component = null
+
+const program = commander
+  .version(version)
+  .option('-f, --functional', 'create a functional component')
+  .option('-t, --type-check [system]', 'add @flow comment to script files')
+  .option('-c, --css-extension [extension]', 'changes the extension of generated css files')
+  .option('-d, --directory [dir]', 'specify a directory for the component')
+  .option('--test [type]', 'either "jest" or "none"')
+  .arguments('<component>')
+  .action((c) => component = c)
+  .parse(process.argv)
+
+if (component == null) {
+  console.error('A component’s name is required.')
+  console.log(`  ${chalk.cyan(program.name())} ${chalk.green('<component>')}`)
+  process.exit(1)
+} else {
+  generate(component, getConfig(program))
+}
--- index.js
+++ index.js
@@ -1,29 +1,19 @@
 #!/usr/bin/env node
+
 'use strict'
 
-const commander = require('commander')
-const chalk = require('chalk')
-const getConfig = require('./config')
-const generate = require('./generate')
-const version = require('./package.json').version
+var chalk = require('chalk')
+var version = process.versions.node
 
-let component = null
-
-const program = commander
-  .version(version)
-  .option('-f, --functional', 'create a functional component')
-  .option('-t, --type-check [system]', 'add @flow comment to script files')
-  .option('-c, --css-extension [extension]', 'changes the extension of generated css files')
-  .option('-d, --directory [dir]', 'specify a directory for the component')
-  .option('--test [type]', 'either "jest" or "none"')
-  .arguments('<component>')
-  .action((c) => component = c)
-  .parse(process.argv)
-
-if (component == null) {
-  console.error('A component’s name is required.')
-  console.log(`  ${chalk.cyan(program.name())} ${chalk.green('<component>')}`)
+if (version.split('.')[0] < 4) {
+  console.error(
+    chalk.red(
+      'You are running Node v' + version + '.\n' +
+      'cra-generate requires Node v4 or higher. \n' +
+      'Please update your version of Node.'
+    )
+  )
   process.exit(1)
-} else {
-  generate(component, getConfig(program))
 }
+
+require('./cli')
