aa72c361521392d276f75252c2faf744c5181876
Ragnar Valgeirsson <ragnar.valgeirsson@gmail.com>
2017-04-09 23:19:07 +0000

    Only support Node version >= 6

--- index.js
+++ index.js
@@ -5,11 +5,11 @@
 var chalk = require('chalk')
 var version = process.versions.node
 
-if (version.split('.')[0] < 4) {
+if (~~version.split('.')[0] < 6) {
   console.error(
     chalk.red(
       'You are running Node v' + version + '.\n' +
-      'cra-generate requires Node v4 or higher. \n' +
+      'cra-generate requires Node v6 or higher. \n' +
       'Please update your version of Node.'
     )
   )
