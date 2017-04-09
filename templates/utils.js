90bb1c8214af1f792e977c0bf90b73a7f97f0421
Ragnar Valgeirsson <ragnar.valgeirsson@gmail.com>
2017-04-09 22:50:00 +0000

    Add a trailing line to files

--- templates/utils.js
+++ templates/utils.js
@@ -1,11 +1,12 @@
 'use strict'
 
 const path = require('path')
+const eol = require('os').EOL
 
 exports.createTemplate = (componentPath, defs) => Object.keys(defs)
   .filter(fileName => defs[fileName])
   .map(fileName => ({
     fileName,
     filePath: path.join(componentPath, fileName),
-    content: defs[fileName] = ('' + defs[fileName]).trim(),
+    content: defs[fileName] = ('' + defs[fileName]).trim() + eol,
   }))
