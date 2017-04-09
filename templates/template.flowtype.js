25e0396793c9a33654f2ce0e33da660ef03b1243
Ragnar Valgeirsson <ragnar.valgeirsson@gmail.com>
2017-04-09 22:51:44 +0000

    Add correct EOL based on OS

--- templates/template.flowtype.js
+++ templates/template.flowtype.js
@@ -1,12 +1,13 @@
 'use strict'
 
 const javascript = require('./template.javascript')
+const eol = require('os').EOL
 const jsRegex = /\.js$/i
 
 module.exports = config => javascript(config)
   .map(file => {
     if (jsRegex.test(file.fileName)) {
-      file.content = `// @flow\n\n${file.content}`
+      file.content = `// @flow${eol}${eol}${file.content}`
     }
     return file
   })
