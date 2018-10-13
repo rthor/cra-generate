ef8a6f9dd16f4d87d2bb2a3cd529336565feed26
alexdriaguine <alexdriagin12@gmail.com>
2018-10-13 12:51:51 +0200

    add typescript template

--- templates/index.js
+++ templates/index.js
@@ -2,12 +2,15 @@
 
 const flowtype = require("./template.flowtype")
 const javascript = require("./template.javascript")
+const typescript = require("./template.typescript")
 
 module.exports = fileType => {
   switch (fileType.toLowerCase()) {
     case "flow":
     case "flowtype":
       return flowtype
+    case "typescript":
+      return typescript
     default:
       return javascript
   }
---
+++ templates/template.typescript.js
@@ -0,0 +1,63 @@
+"use strict"
+const utils = require("./utils")
+
+module.exports = ({
+  componentPath,
+  componentName,
+  fileName,
+  semiColon,
+  cssExtension,
+  noTest,
+  isFunctional,
+}) =>
+  utils.createTemplate(componentPath, {
+    [`${fileName}.${cssExtension}`]: `
+.${componentName} {}
+  `,
+
+    "index.ts": `
+export { default } from './${fileName}'${semiColon}
+  `,
+
+    [`${fileName}.test.tsx`]: noTest
+      ? ""
+      : `
+import * as React from 'react'${semiColon}
+import * as ReactDOM from 'react-dom'${semiColon}
+import ${componentName} from './${fileName}'${semiColon}
+
+it('renders without crashing', () => {
+  const div = document.createElement('div')${semiColon}
+  ReactDOM.render(<${componentName} />, div)${semiColon}
+})${semiColon}
+  `,
+
+    [`${fileName}.tsx`]: isFunctional
+      ? `
+import * as React from 'react'${semiColon}
+import './${fileName}.${cssExtension}'${semiColon}
+
+const ${componentName}: React.SFC = ({}) => (
+  <div className="${componentName}"></div>
+)${semiColon}
+
+export default ${componentName}${semiColon}
+
+  `
+      : `
+import * as React from 'react'${semiColon}
+import './${fileName}.${cssExtension}'${semiColon}
+
+class ${componentName} extends React.Component<{}, {}> {
+  public state = {}${semiColon}
+
+  public render() {
+    return (
+      <div className="${componentName}" />
+    )${semiColon}
+  }
+}
+
+export default ${componentName}${semiColon}
+  `,
+  })
